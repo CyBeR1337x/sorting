8543eaced3cf5df8d5205dbab96757bb33e56bb2
Syed Shah Meer Haider <60551634+CyBeR1337x@users.noreply.github.com>
2022-02-02 01:21:46 +0500

    Create selection_sort(descending order).cpp

---
+++ selection_sort(descending order).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int[], int);
+
+void main() {
+	cout << "Selection sort(descending order)..." << endl;
+
+	int size;
+	cout << "Enter Size of list: ";
+	cin >> size;
+
+	int* list = new int[size];
+
+	cout << "Enter elements of array: ";
+	for (int i = 0; i < size; i++)
+		cin >> list[i];
+
+	cout << "Unsorted List: [ ";
+	printArray(list, size);
+	cout << "]" << endl;
+
+	//Selection sort
+	for (int i = (size - 1); i >= 1; i--) {
+		int max = 0;
+		int j = 1;
+
+		for (j; j <= i; j++) {
+			if (list[max] > list[j])
+				max = j;
+		}
+		//Swapping
+		int temp;
+		temp = list[i];
+		list[i] = list[max];
+		list[max] = temp;
+	}
+
+	cout << "Sorted List: [ ";
+	printArray(list, size);
+	cout << "]" << endl;
+}
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
